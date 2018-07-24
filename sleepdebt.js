5b6b1e074e08eeeb5ae8a234fa96d5cf4de61a75
Piotr Józefów <piotr.jozefow@gmail.com>
2018-07-24 11:49:57 +0200

    refactored sleepdebt.js

--- sleepdebt.js
+++ sleepdebt.js
@@ -1,51 +1,51 @@
+const DAYS_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 const getSleepHours = day => {
-  if (day === 'Monday') {
-    return 8;
-  } else if (day === 'Tuesday') {
-    return 6;
-  } else if (day === 'Wednesday') {
-    return 7;
-  } else if (day === 'Thursday') {
-    return 7;
-  } else if (day === 'Friday') {
-    return 8;
-  } else if (day === 'Saturday') {
-    return 6;
-  } else if (day === 'Sunday') {
-    return 5;
+  switch(day) {
+    case 'Monday':
+      return 8;
+    case 'Tuesday':
+      return 6;
+    case 'Wednesday':
+      return 7;
+    case 'Thursday':
+      return 7;
+    case 'Friday':
+      return 8;
+    case 'Saturday':
+      return 6;
+    case 'Sunday':
+      return 5;
+    default:
+      throw new Error('Invalid day.')
   }
 };
 
-const getActualSleepHours = () =>
-  getSleepHours('Monday') +
-  getSleepHours('Tuesday') +
-  getSleepHours('Wednesday') +
-  getSleepHours('Thursday') +
-  getSleepHours('Friday') +
-  getSleepHours('Saturday') +
-  getSleepHours('Sunday');
+const getActualSleepHours = (daysArray) => {
+  let hours = 0;
+  for(let day of daysArray) {
+    hours = hours + getSleepHours(day)
+  }
+  return hours;
+}
 
 const getIdealSleepHours = () => {
-  const idealHours = 8; // po co zmienna? nie lepiej po prostu 8 * 7?
-  return idealHours * 7;
+  const idealHours = 8;
+  const daysInWeek = 7;
+  return idealHours * daysInWeek;
 };
 
 const calculateSleepDebt = () => {
-  const actualSleepHours = getActualSleepHours();
+  const actualSleepHours = getActualSleepHours(DAYS_OF_WEEK);
   const idealSleepHours = getIdealSleepHours();
-  if (actualSleepHours === idealSleepHours) {
-    return 'You had the perfect amount of sleep';
-  } else if (actualSleepHours > idealSleepHours) {
-    return `You got ${actualSleepHours-idealSleepHours} hour(s) more sleep than you needed this week.`;
-  } else if (actualSleepHours < idealSleepHours) {
-      return `You got ${idealSleepHours-actualSleepHours} hour(s) less sleep than you needed this week. Get some rest.`;
-  } else {
-    return 'Error';
+
+  if (actualSleepHours < idealSleepHours) {
+    return `You got ${idealSleepHours - actualSleepHours} hour(s) less sleep than you needed this week. Get some rest.`;
   }
-};
+  if (actualSleepHours > idealSleepHours) {
+    return `You got ${actualSleepHours - idealSleepHours} hour(s) more sleep than you needed this week.`;
+  }
+  return 'You had the perfect amount of sleep';
+}
 
 console.log(calculateSleepDebt());
-
-
-// console log nic nie zwraca:
-console.log(console.log('XX'));
